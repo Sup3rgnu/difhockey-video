7ab2701f2f63d1452fe544821db84603334ba25b
Mikael Falgard <mikael.falgard@valtech.se>
2015-10-17 01:10:21 +0200

    Added route to display videos on page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var fs = require('fs');
+var fs 		= require('fs');
 var request = require('request');
+var async 	= require('async');
 var cheerio = require('cheerio');
-var app     = express();
+var app 	= express();
 
 app.get('/scrape', function(req, res){
 
@@ -31,30 +32,80 @@ app.get('/scrape', function(req, res){
 			        	url = link.attr('href');
 
 			        	if (title.indexOf("Highlights") > -1) {
-				        	json.highlights.push({
-			        			"title" : title,
-	        					"url" : "http://difhockey.se" + url
-			        		});
-			        	} else {
+			        		// pushing to same list at the moment
 				        	json.videos.push({
 			        			"title" : title,
 	        					"url" : "http://difhockey.se" + url
 			        		});
-			        	}
+			        	} else {
+							json.videos.push({
+								"title" : title,
+								"url" : "http://difhockey.se" + url
+						});
+					}
 
-		        	}
+					}
 				});
 
 				///console.log(json);
-	        })
+			})
 		}
 
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        })
+		fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err, data){
+			if (err) throw err;
 
-        res.send('Check your console!')
+			console.log('File successfully written! - Check your project directory for the output.json file');
+			console.log(data);
+		})
+
+		res.send('Check your console!')
 	})
+})
+
+app.get('/videos', function(req, res){
+	var videos = [];
+
+	fs.readFile('output.json', 'utf8', function (err, data) {
+		if (err) throw err;
+		var json = JSON.parse(data);
+		var list = json.videos;
+
+		async.forEach(list, function (video, callback) {
+			request(video.url, function (error, response, html){
+				if (error) throw error;
+
+				var $ = cheerio.load(html);
+				var url = $('.esContent iframe').first().attr('src');
+				videos.push({ 
+					"url" : url, 
+					"title" : video.title
+				});
+				callback();
+			});
+
+		}, function (err) {
+			if (err) return next(err);
+
+//			fs.writeFile('videos.json', JSON.stringify(videos, null, 4), function (err, data){
+//				if (err) throw err;
+//				console.log('File successfully written! - Check your project directory for the output.json file');
+//			})
+
+			var htmlStart = '<html><head><style>div{display:block;float:left;width:24%;}iframe{width:100%;height:200px}h4{height:50px}</style></head><body>';
+			var htmlEnd = '</body></html>';
+			var videoFrames = '';
+			var videoTitles = '';
+
+			for ( var i in videos) {
+				videoFrames += '<div><iframe src="' + videos[i].url + '"></iframe><h4>' + videos[i].title + '</h4></div>'
+			}
+
+			res.send(htmlStart + videoFrames + htmlEnd);
+		});
+
+
+	});
+
 })
 
 app.listen('8081')
